a5665dcc269cd74a24b14cc1a6fa6d38c82d59ed
Brian Schlining <bschlining@gmail.com>
2020-06-12 15:22:29 -0700

    M3-128: Prepping release

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# build script used internally at MBARI
+
+export JPACKAGE_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-14.jdk/Contents/Home"
+source "$HOME/workspace/m3-deployspace/cthulhu/env-config.sh"
+gradlew clean jpackage --info

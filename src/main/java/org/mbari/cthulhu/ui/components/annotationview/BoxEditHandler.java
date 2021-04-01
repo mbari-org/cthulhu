9caf529c7a296e153c4002a99c4ca8f0935e98d2
Carlos Rueda <carueda@mbari.org>
2021-03-31 19:42:33 -0700

    add box repositioning, and more refinements

--- src/main/java/org/mbari/cthulhu/ui/components/annotationview/BoxEditHandler.java
+++ src/main/java/org/mbari/cthulhu/ui/components/annotationview/BoxEditHandler.java
@@ -2,6 +2,7 @@ package org.mbari.cthulhu.ui.components.annotationview;
 
 import javafx.event.EventHandler;
 import javafx.geometry.BoundingBox;
+import javafx.scene.Cursor;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -10,9 +11,7 @@ import javafx.scene.shape.StrokeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Helper for editing of existing box.
@@ -22,8 +21,13 @@ public class BoxEditHandler {
      * Interface for AnnotationImageView to react to events associated with the editing.
      */
     interface Listener {
+        
         void startDragRectangle(double anchorX, double anchorY, double width, double height, double x, double y);
+        
         void continueDragRectangle(double x, double y);
+        
+        void moveDragRectangle(double x, double y, double w, double h);
+        
         void completeDragRectangle(UUID id);
     }
 
@@ -37,6 +41,23 @@ public class BoxEditHandler {
     };
     
     private final Listener listener;
+    
+    // Info to control the repositioning of the rectangle (not resizing)
+    private static class MoveInfo {
+        final BoundingBox bb;
+        final double mouseX;
+        final double mouseY;
+        boolean dragging = false;
+        
+        public MoveInfo(BoundingBox bb, double mouseX, double mouseY) {
+            this.bb = bb;
+            this.mouseX = mouseX;
+            this.mouseY = mouseY;
+        }
+    }
+    
+    // Non-null only while dragging rectangle
+    private MoveInfo moveInfo = null;
     
     // Non-null only when this handler is active
     private UUID id = null;
@@ -86,16 +107,30 @@ public class BoxEditHandler {
             Circle corner = corners[index];
             corner.setCenterX(x);
             corner.setCenterY(y);
-            corner.setVisible(true);
             corner.toFront();
         }
+        setCornersVisible(true);
         this.id = id;
     }
     
     void deactivateHandling() {
-        this.id = null;
+        if (isActive() && dragging()) {
+            corners[0].getScene().setCursor(Cursor.DEFAULT);
+        }
+        setCornersVisible(false);
+        id = null;
+        moveInfo = null;
+    }
+    
+    private void setCornersVisible(boolean visible) {
         for (Circle corner : corners) {
-            corner.setVisible(false);
+            corner.setVisible(visible);
+        }
+    }
+    
+    private void setCornersToFront() {
+        for (Circle corner : corners) {
+            corner.toFront();
         }
     }
     
@@ -103,6 +138,70 @@ public class BoxEditHandler {
         return id != null;
     }
     
+    // mouse* methods below for handling repositioning (not resizing)
+    
+    private boolean dragging() {
+        return moveInfo != null && moveInfo.dragging;
+    }
+    
+    void mousePressed(double mouseX, double mouseY) {
+        assert(isActive());
+
+        final BoundingBox bb = getBoundingBox();
+        final boolean inside = bb.contains(mouseX, mouseY);
+        if (inside) {
+            // prepare for possible dragging:
+            log.debug("mousePressed: bb={} mouseX={} mouseY={}", bb, mouseX, mouseY);
+            moveInfo = new MoveInfo(bb, mouseX, mouseY);
+            corners[0].getScene().setCursor(Cursor.CLOSED_HAND);
+        }
+        else {
+            moveInfo = null;
+        }
+    }
+
+    void mouseDragged(double mouseX, double mouseY) {
+        if (moveInfo != null) {
+            log.debug("mouseDragged mouseX={} mouseY={}", mouseX, mouseY);
+            final double deltaX = mouseX - moveInfo.mouseX;
+            final double deltaY = mouseY - moveInfo.mouseY;
+            moveInfo.dragging = moveInfo.dragging || deltaX != 0 || deltaY != 0;
+            if (moveInfo.dragging) {
+                double x = moveInfo.bb.getMinX() + deltaX;
+                double y = moveInfo.bb.getMinY() + deltaY;
+                double w = moveInfo.bb.getWidth();
+                double h = moveInfo.bb.getHeight();
+                setCornersVisible(false);
+                listener.moveDragRectangle(x, y, w, h);
+            }
+        }
+    }
+    
+    void mouseReleased() {
+        if (dragging()) {
+            log.debug("mouseReleased: box edit is active");
+            listener.completeDragRectangle(id);
+            deactivateHandling();
+        }
+    }
+    
+    private BoundingBox getBoundingBox() {
+        double minX = Double.MAX_VALUE, minY = Double.MAX_VALUE;
+        double maxX = Double.MIN_VALUE, maxY = Double.MIN_VALUE;
+        for (Circle corner : corners) {
+            double x = corner.getCenterX();
+            double y = corner.getCenterY();
+            if (minX > x) minX = x;
+            if (minY > y) minY = y;
+            if (maxX < x) maxX = x;
+            if (maxY < y) maxY = y;
+        }
+        return new BoundingBox(minX, minY, maxX - minX, maxY - minY);
+    }
+    
+    /**
+     * Handler for resizing purposes.
+     */
     private class CornerMouseEventHandler implements EventHandler<MouseEvent> {
         private final int index;
         
@@ -126,6 +225,7 @@ public class BoxEditHandler {
                 final double x = event.getX();
                 final double y = event.getY();
                 listener.startDragRectangle(anchorX, anchorY, width, height, x, y);
+                setCornersToFront();
             }
             else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
                 final double x = event.getX();
